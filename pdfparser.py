cfb79898d1806b5f3fe9cdc5f3bdae72e796d5d0
Omer Ahmed <omeryosif.ai@gmail.com>
2023-09-16 18:52:21 -0400

    Update pdfparser.py

--- pdfparser.py
+++ pdfparser.py
@@ -7,37 +7,54 @@ vesion: 09/16/2023
 
 # Imports
 from pypdf import PdfReader as pdfReader
+import openai
+import os
 
-# Getting location of resume and reading it
-path = input("Enter the PATH of the resume: ")
-reader = pdfReader(path)
+reader = pdfReader(r"resume_path")
 
 # Matchability variables
 skills = {
-    'programming_languages': [],
-    'frameworks': [],
-    'OS': [],
+  'programming_languages': [],
+  'frameworks': [],
+  'OS': [],
 }
 
 grade = None # Year of candidate
-gpa = None # GPA of candidate
+gpa = 0 # GPA of candidate
 citizen = None # Citizenship status of candidate
 degree = None # CS, CPE, or EE degree?
 experience = None # Experience of candidate (determined by internships and jobs)
 
+# Setting up ChatGPT
+openai.api_key = "yourapikey"
+
 # Loop through pages
 for i in range(len(reader.pages)):
-    page = reader.pages[i]
+  page = reader.pages[i]
 
-    # Create resume file
-    open("resume", "w+")
+  # Create resume file
+  open("resume.txt", "w+")
 
-    # Write to resume txt file
-    with open("resume", "w", encoding="utf-8") as resume:
-        text = page.extract_text()
-        resume.write(text)
+  # Write to resume txt file
+  with open("resume.txt", "w", encoding="utf-8") as resume_file:
+    text = page.extract_text()
+    resume_file.write(text)
 
-    # Reading resume txt file
-    with open("resume", "r", encoding="utf-8") as resume:
-        for line in resume:
-            print(line)
+  # Reading resume txt file
+  with open("resume.txt", "r", encoding="utf-8") as resume:
+    resume_text = "".join(resume.readlines())
+
+    # Request GPT-3.5 Turbo to extract technical skills from the resume text
+    prompt = "Please extract technical skills and courses from the given resume text:\n\n" + resume_text
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+          {"role": "system", "content": prompt},
+      ],
+      temperature = 0
+    )
+
+    candidate_knowledge = completion.choices[0].message.content
+
+# Matchability Percentage = (Total Relevant Skills / Total Required Skills) ...
+
