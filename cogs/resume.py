7082667e7048855f58242d57bd126e7d062c2750
Muhammad Nabil <coronhaus@gmail.com>
2023-09-16 21:16:34 -0400

    read command finished

--- cogs/resume.py
+++ cogs/resume.py
@@ -9,18 +9,27 @@ import discord
 from discord.commands import slash_command
 from .base_cog import CCog
 from pypdf import PdfReader
-import requests
+from datetime import datetime
 
 class ResumeCog(CCog):
     """
-    Contains the main functions of the bot from parsing resumes and claculating eligibility for the job.
+    Contains the main functions of the bot such as parsing resumes.
     """
 
-    @slash_command(name="readresume", description="Takes in a resume attachment file.", guilds=[])
+    @slash_command(name="readresume", description="Reads a PDF attachment file.", guilds=[1152451000079220897])
     async def read(self, ctx : discord.ApplicationContext,
-                   resume : discord.Option(discord.SlashCommandOptionType.attachment, name="resume",
-                                           description="The resume to scan (PDF format)")):
-        
+                   resume : discord.Option(discord.SlashCommandOptionType.attachment,
+                                           name="resume",
+                                           description="The resume to scan (PDF format)"),
+                    verbose : discord.Option(discord.SlashCommandOptionType.boolean,
+                                             name="verbose",
+                                             description="Whether to display extra PDF information or not.",
+                                             default = False)):
+        """
+        Interprets the text of the attached PDF file along with its metadata.
+        Debugging command.
+        """
+
         # Check if it is a PDF file
         url = resume.url
         if not url.endswith('pdf'):
@@ -31,4 +40,45 @@ class ResumeCog(CCog):
 
         content = await resume.read()
 
-        reader = PdfReader(content)
+        # Write the PDF
+        with open('metadata.pdf', 'wb') as pdf:
+            pdf.write(content)
+        
+        with open('metadata.pdf', 'rb') as pdf:
+            reader = PdfReader(pdf)
+            md = reader.metadata
+
+            # Send verbose info
+            if verbose:
+                infoEmbed = discord.Embed(title=resume.filename)
+                infoEmbed.set_author(name=(md.author or 'Unknown author'))
+
+                infoEmbed.add_field(
+                    name="Title",
+                    value=(md.title or 'None'),
+                    inline=False
+                )
+
+                infoEmbed.add_field(
+                    name="Creator",
+                    value=(md.creator or 'Not specified')
+                )
+
+                infoEmbed.add_field(
+                    name="Producer",
+                    value=(md.producer or 'Not specified')
+                )
+
+                infoEmbed.add_field(
+                    name="Creation Date",
+                    value=str(md.creation_date or 'Not specified'),
+                    inline=False
+                )
+
+                await ctx.channel.send(embed=infoEmbed)
+            
+            # Parse text
+            for i in range(len(reader.pages)):
+                page = reader.pages[i]
+                text = page.extract_text()
+                await ctx.channel.send(f'Text excerpt from page {i+1}: ```{text[0:1920]}```')
