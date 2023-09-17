2a71f462fdea004ecc0a445de5e067a39c34cbf3
Muhammad Nabil <coronhaus@gmail.com>
2023-09-16 20:04:41 -0400

    PDF Reading

--- client.py
+++ client.py
@@ -3,19 +3,17 @@ Main driver code for the Discord bot.
 Profile picture is attributed to Freepik.
 
 author: Muhammad Nabil (myn55)
-version: 9/16/2023
+version: 09/16/2023
 """
 
 import discord, json
 from cogs.maintenance import MaintenanceCog
 from cogs.help import HelpCog
+from cogs.resume import ResumeCog
 from colorama import init, Fore, Back, Style
 init()
 
 class ResumeBot(discord.Bot):
-    def __init__(self, description=None, *args, **options) -> None:
-        super().__init__(description, *args, **options)
-
     async def on_ready(self) -> None:
         print(Fore.GREEN+f"Successfully logged in as {Fore.CYAN+Style.BRIGHT+str(self.user)}"+Style.RESET_ALL)
 
@@ -24,10 +22,12 @@ intents = discord.Intents()
 intents.members = True
 intents.guild_messages = True
 intents.message_content = True
+intents.guilds = True
 
 client = ResumeBot(intents=intents)
 with open("config.json", "r") as config:
     client.add_cog(MaintenanceCog())
+    client.add_cog(ResumeCog())
     client.add_cog(HelpCog())
 
     client.run(json.load(config)["token"])
--- cogs/resume.py
+++ cogs/resume.py
@@ -1,15 +1,34 @@
 """
 Contains the ResumeCog class.
 
-author: Muhammad Nabil (myn55)
-version: 9/16/2023
+author: Omer Ahmed (omerahmed05), Jonathan Woodbury (jonathanw22), Muhammad Nabil (myn55)
+version: 09/16/2023
 """
 
 import discord
 from discord.commands import slash_command
 from .base_cog import CCog
+from pypdf import PdfReader
+import requests
 
 class ResumeCog(CCog):
     """
     Contains the main functions of the bot from parsing resumes and claculating eligibility for the job.
     """
+
+    @slash_command(name="readresume", description="Takes in a resume attachment file.", guilds=[])
+    async def read(self, ctx : discord.ApplicationContext,
+                   resume : discord.Option(discord.SlashCommandOptionType.attachment, name="resume",
+                                           description="The resume to scan (PDF format)")):
+        
+        # Check if it is a PDF file
+        url = resume.url
+        if not url.endswith('pdf'):
+            await ctx.respond(content="The file you provided is not a PDF.", ephemeral=True)
+            return
+        
+        await ctx.respond("Reading PDF...")
+
+        content = await resume.read()
+
+        reader = PdfReader(content)
