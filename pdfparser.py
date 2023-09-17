77afb1dbb5819e7aec0cd15b23d146d122bfbf37
Omer Ahmed <omeryosif.ai@gmail.com>
2023-09-17 00:07:33 -0400

    Update pdfparser.py

--- pdfparser.py
+++ pdfparser.py
@@ -12,21 +12,6 @@ import os
 
 reader = pdfReader(r"resume_path")
 
-# Matchability variables
-skills = {
-  'programming_languages': [],
-  'frameworks': [],
-  'OS': [],
-}
-
-grade = None # Year of candidate
-gpa = 0 # GPA of candidate
-citizen = None # Citizenship status of candidate
-degree = None # CS, CPE, or EE degree?
-experience = None # Experience of candidate (determined by internships and jobs)
-
-# Setting up ChatGPT
-openai.api_key = "yourapikey"
 
 # Loop through pages
 for i in range(len(reader.pages)):
@@ -43,18 +28,7 @@ for i in range(len(reader.pages)):
   # Reading resume txt file
   with open("resume.txt", "r", encoding="utf-8") as resume:
     resume_text = "".join(resume.readlines())
+    print(resume_text)
+  
 
-    # Request GPT-3.5 Turbo to extract technical skills from the resume text
-    prompt = "Please extract technical skills and courses from the given resume text:\n\n" + resume_text
-    completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-          {"role": "system", "content": prompt},
-      ],
-      temperature = 0
-    )
-
-    candidate_knowledge = completion.choices[0].message.content
-
-# Matchability Percentage = (Total Relevant Skills / Total Required Skills) ...
-
+    
